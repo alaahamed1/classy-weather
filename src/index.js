d65b51da42446f6752f0770ea5f520de63c7eb65
alaahamed22 <alaa.hamed8000@gmail.com>
2023-10-02 21:37:10 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Appv2 from './Appv2';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Appv2 />
+  </React.StrictMode>
+);
